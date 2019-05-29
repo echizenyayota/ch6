a8e710b64edac65d2cf650ea3917d55b0fff0c2d
echizenyayota <virtualmisso30@gmail.com>
2019-05-29 13:09:28 +0900

    144 静的メソッドを定義したい

---
+++ scripts/class_static.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+    class Figure {
+        static getSquareArea(base, height) {
+            return base * height;
+        }
+    }
+
+    console.log(Figure.getSquareArea(5, 3));
+
+}
